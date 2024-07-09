d7480bb9bcffc3015e05edcdd2fb76df07f1fb95
Mathewzachariah <mathewzchrh603@gmail.com>
2024-07-09 13:37:23 +0530

    user details page redesign

--- app/src/main/java/com/example/myapplication/UserDetailsActivity.kt
+++ app/src/main/java/com/example/myapplication/UserDetailsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
+import android.graphics.Color
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,10 +9,8 @@ import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.ScrollView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.myapplication.apiManager.model.User
@@ -20,7 +19,7 @@ class UserDetailsActivity : AppCompatActivity()
 {
     lateinit var mainActivityViewModel: MainActivityViewModel
     lateinit var progressBar: ProgressBar
-    lateinit var userDetailsParentScrollView: ScrollView
+    lateinit var userDetailsParentScrollView: ConstraintLayout
 
 
     lateinit var userImageView: ImageView
@@ -35,6 +34,11 @@ class UserDetailsActivity : AppCompatActivity()
     lateinit var accountUsernameTextView: TextView
     lateinit var bloodTypeTextView: TextView
     lateinit var accountPasswordTextView: TextView
+    lateinit var genderImageView: ImageView
+    lateinit var addressTextView: TextView
+    lateinit var homeTextView: TextView
+    lateinit var companyTextView: TextView
+    lateinit var bankTextView: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?)
@@ -43,7 +47,8 @@ class UserDetailsActivity : AppCompatActivity()
         setContentView(R.layout.activity_user_details)
         init()
         mainActivityViewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
-        retrieveUserDetailsById(intent.extras!!.getInt("UserId", 0))
+        // retrieveUserDetailsById(intent.extras!!.getInt("UserId", 1))
+        retrieveUserDetailsById(1)
     }
 
     fun init(): Unit
@@ -55,13 +60,18 @@ class UserDetailsActivity : AppCompatActivity()
         hairTextView = findViewById(R.id.hair)
         userBirthDayTextView = findViewById(R.id.birthday)
         bloodTypeTextView = findViewById(R.id.bloodtype)
-        emailTextView = findViewById<TextView>(R.id.email)
-        heightTextView = findViewById<TextView>(R.id.height)
-        weightTextView = findViewById<TextView>(R.id.weight)
-        phoneTextView = findViewById<TextView>(R.id.phone)
-        accountUsernameTextView = findViewById<TextView>(R.id.account_username)
-        accountPasswordTextView = findViewById<TextView>(R.id.account_password)
-        userImageView = findViewById<ImageView>(R.id.userimage)
+        emailTextView = findViewById(R.id.email)
+        heightTextView = findViewById(R.id.height)
+        weightTextView = findViewById(R.id.weight)
+        phoneTextView = findViewById(R.id.phone)
+        accountUsernameTextView = findViewById(R.id.account_username)
+        accountPasswordTextView = findViewById(R.id.account_password)
+        genderImageView = findViewById(R.id.gender)
+        homeTextView = findViewById(R.id.home)
+        companyTextView = findViewById(R.id.company)
+        bankTextView = findViewById(R.id.bank)
+        addressTextView = findViewById(R.id.address)
+        userImageView = findViewById(R.id.userimage)
     }
 
     private fun retrieveUserDetailsById(userId: Int)
@@ -84,35 +94,95 @@ class UserDetailsActivity : AppCompatActivity()
                         userDetails.firstName + " " + (if (userDetails.maidenName.isNotEmpty()) (userDetails.maidenName.first() + " ") else "") + userDetails.lastName
                     userNameTextView.text = name
                     eyeColorTextView.text = buildString {
-                        append("Eye Color")
-                        append(" : ")
+
                         append(userDetails.eyeColor)
                     }
 
+                    homeTextView.setOnClickListener {
+
+
+                        homeTextView.setTextColor(Color.RED)
+                        bankTextView.setTextColor(Color.BLACK)
+                        companyTextView.setTextColor(Color.BLACK)
+                        addressTextView.text = buildString {
+                            userDetails.address?.let { address ->
+                                append(address.address)
+                                append(",")
+                                append(address.city)
+                                append(",")
+                                append(address.state)
+                                append(",")
+                                append(address.country)
+                            }
+
+                        }
+                    }
+                    homeTextView.performClick()
+                    companyTextView.setOnClickListener {
+                        companyTextView.setTextColor(Color.RED)
+                        bankTextView.setTextColor(Color.BLACK)
+                        homeTextView.setTextColor(Color.BLACK)
+                        addressTextView.text = buildString {
+                            userDetails.company?.let { company ->
+                                append(company.name)
+                                append("\n")
+                                append(company.title)
+                                append("\n")
+                                append(company.department)
+                                append("\n\n")
+                                company.address?.let { companyAddress ->
+                                    append(companyAddress.address)
+                                    append(",")
+                                    append(companyAddress.city)
+                                    append(",")
+                                    append(companyAddress.state)
+                                    append(",")
+                                    append(companyAddress.country)
+                                }
+
+                            }
+
+                        }
+                    }
+                    bankTextView.setOnClickListener {
+                        homeTextView.setTextColor(Color.BLACK)
+                        companyTextView.setTextColor(Color.BLACK)
+                        bankTextView.setTextColor(Color.RED)
+
+
+                        addressTextView.text = buildString {
+                            userDetails.bank?.let { bankDetails ->
+                                append(bankDetails.cardNumber)
+                                append("\n")
+                                append(bankDetails.cardType)
+                                append("\n")
+                                append(bankDetails.cardExpire)
+                                append(",")
+                                append(bankDetails.currency)
+                            }
+
+                        }
+                    }
+                    val genderIconImage = if (userDetails.gender == "female") R.drawable.baseline_female_24 else R.drawable.baseline_male_24
                     hairTextView.text = buildString {
-                        append("Hair ")
-                        append(" : ")
+
                         append(userDetails.hair!!.color)
                         append(" ")
                         append(userDetails.hair!!.type)
                     }
                     emailTextView.text = userDetails.email
                     heightTextView.text = buildString {
-                        append("Height")
-                        append(" : ")
+
                         append(userDetails.height)
                     }
 
                     weightTextView.text = buildString {
-                        append("Weight")
-                        append(" : ")
+
                         append(userDetails.weight)
                     }
                     phoneTextView.text = userDetails.phone
                     accountUsernameTextView.text = userDetails.username
                     bloodTypeTextView.text = buildString {
-                        append("Blood Group")
-                        append(" : ")
                         append(userDetails.bloodGroup)
                     }
                     userBirthDayTextView.text = buildString {
@@ -127,6 +197,8 @@ class UserDetailsActivity : AppCompatActivity()
 
                     val defaultIcon = if (userDetails.gender == "female") R.drawable.female_default_icon else R.drawable.male_default_icon
                     Glide.with(this@UserDetailsActivity).load(userDetails.image).placeholder(defaultIcon).into(userImageView);
+                    Glide.with(this@UserDetailsActivity).load(genderIconImage).into(genderImageView);
+
                 }
             }
 
